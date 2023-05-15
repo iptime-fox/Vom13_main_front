c505170882c4bb6ada1680ce6c036d3fab4993f5
iptime-fox <dudndi826@gmail.com>
2023-05-15 12:47:06 +0900

    로그인, 로그아웃 엔드포인트 추가 0515

--- js/etc/endPoints.js
+++ js/etc/endPoints.js
@@ -1,6 +1,8 @@
 var endPoints = {
   register: {
     signup: 'http://hanara123.dothome.co.kr/baexang_back/register/signup',
+    signin: 'http://hanara123.dothome.co.kr/baexang_back/register/signin',
+    signout: 'http://hanara123.dothome.co.kr/baexang_back/register/signout',
   },
   admin: {},
   productPictures: {},
